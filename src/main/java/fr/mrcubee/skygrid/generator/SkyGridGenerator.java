f06b8e6cd71bca7ede1b6af23b958dd96408ba75
MrCubee <contact@mrcubee.fr>
2026-07-22 05:47:51 +0200

    feat: port generator to modern Paper 26.2 API
    Migrate the SkyGrid generator off the legacy pre-1.13 Bukkit API and
    onto the modern Paper 26.2 chunk generation API.
    
    Generator:
    - Replace the removed byte[] generate() with the staged
      generateNoise/Surface/Bedrock/Caves methods and ChunkData.setBlock.
    - Rebuild block/item/creature filtering with modern APIs: skip legacy
      materials, drop ticking block entities from the grid (keeping chests),
      and route pressure plates, doors, signs, etc. to container loot.
    - Resolve banned materials by name so missing constants no longer break
      compilation across versions.
    - Force generated leaves to be persistent and pistons to be retracted so
      they don't decay or update on their own.
    - Migrate CreatureType to EntityType, keeping only Mob-based types.
    
    Spawner populator:
    - Switch MOB_SPAWNER to SPAWNER and call update() so changes persist.
    - Allow spawners to generate empty (no spawned type).
    
    Bump version to 1.2.

--- src/main/java/fr/mrcubee/skygrid/generator/SkyGridGenerator.java
+++ src/main/java/fr/mrcubee/skygrid/generator/SkyGridGenerator.java
@@ -1,18 +1,26 @@
 package fr.mrcubee.skygrid.generator;
 
-import fr.mrcubee.skygrid.generator.populator.ContainerPopulator;
-import fr.mrcubee.skygrid.generator.populator.SpawnerPopulator;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.CreatureType;
-import org.bukkit.generator.BlockPopulator;
-import org.bukkit.generator.ChunkGenerator;
-
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Leaves;
+import org.bukkit.block.data.type.Piston;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Mob;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+
+import fr.mrcubee.skygrid.generator.populator.ContainerPopulator;
+import fr.mrcubee.skygrid.generator.populator.SpawnerPopulator;
 
 /**
  * @author MrCubee
@@ -20,42 +28,77 @@ import java.util.stream.Collectors;
  */
 public class SkyGridGenerator extends ChunkGenerator {
 
+    private static Stream<Material> byName(final String... names) {
+        return Stream.of(names)
+        .map(Material::matchMaterial)
+        .filter(Objects::nonNull);
+    }
+
+    private static final List<Material> BAN_BLOCK_ENTITY = byName(
+        // Containers / storage
+        "BARREL", "DISPENSER", "DROPPER", "HOPPER",
+        "CHISELED_BOOKSHELF", "DECORATED_POT", "CRAFTER", "LECTERN",
+
+        // Furnaces & cooking
+        "FURNACE", "BLAST_FURNACE", "SMOKER",
+
+        // Redstone / mechanisms
+        "COMPARATOR", "DAYLIGHT_DETECTOR", "JUKEBOX",
+
+        // Spawners / 1.21
+        //"SPAWNER", "TRIAL_SPAWNER", "VAULT",
+
+        // Brewing / beacons / bell / enchanting
+        "BREWING_STAND", "BEACON", "CONDUIT", "BELL", "ENCHANTING_TABLE",
+
+        // Sculk
+        "SCULK_SENSOR", "CALIBRATED_SCULK_SENSOR", "SCULK_SHRIEKER", "SCULK_CATALYST",
+
+        // Bees / brushable
+        "BEEHIVE", "BEE_NEST", "SUSPICIOUS_SAND", "SUSPICIOUS_GRAVEL",
+
+        // Heads
+        "SKELETON_SKULL", "WITHER_SKELETON_SKULL", "ZOMBIE_HEAD",
+        "PLAYER_HEAD", "CREEPER_HEAD", "DRAGON_HEAD", "PIGLIN_HEAD",
+
+        // Misc / technical / 26.x additions
+        "CREAKING_HEART", "COPPER_GOLEM_STATUE", "END_GATEWAY",
+        "COMMAND_BLOCK", "CHAIN_COMMAND_BLOCK", "REPEATING_COMMAND_BLOCK",
+        "JIGSAW", "STRUCTURE_BLOCK"
+    ).toList();
+
+
+    private static final List<Material> KEEP_CONTAINERS = byName("CHEST", "TRAPPED_CHEST").toList();
+
+    private static boolean isBlockEntity(final Material material) {
+        return BAN_BLOCK_ENTITY.contains(material)
+            || Tag.SIGNS.isTagged(material)
+            || Tag.ALL_HANGING_SIGNS.isTagged(material)
+            || Tag.BANNERS.isTagged(material)
+            || Tag.BEDS.isTagged(material)
+            || Tag.SHULKER_BOXES.isTagged(material)
+            || Tag.CAMPFIRES.isTagged(material);
+    }
+
     /**
      * List of materials whose installation is prohibited in the world.
      */
     private static final List<Material> BAN_BLOCK_LIST = Arrays.asList(
             Material.AIR,
             Material.BEDROCK,
-            Material.COMMAND,
-            Material.PISTON_EXTENSION,
-            Material.PISTON_MOVING_PIECE,
-            Material.GLOWING_REDSTONE_ORE,
             Material.WATER,
-            Material.STATIONARY_WATER,
             Material.LAVA,
-            Material.STATIONARY_LAVA,
-            Material.BURNING_FURNACE,
-            Material.REDSTONE_LAMP_ON,
-            Material.SIGN_POST,
-            Material.WALL_SIGN,
-            Material.IRON_DOOR_BLOCK,
-            Material.ENDER_PORTAL,
-            Material.PORTAL,
-            Material.ENDER_PORTAL_FRAME,
             Material.BEACON,
             Material.BREWING_STAND,
-            Material.BREWING_STAND_ITEM,
             Material.BARRIER,
-            Material.BED_BLOCK,
             Material.DRAGON_EGG,
-            Material.COMMAND_MINECART,
             Material.FIRE,
-            Material.STANDING_BANNER,
-            Material.WALL_BANNER,
             Material.BOOK,
             Material.ENCHANTED_BOOK,
             Material.WRITTEN_BOOK,
-            Material.MONSTER_EGG
+            Material.TRIAL_SPAWNER,
+            Material.VAULT,
+            Material.END_PORTAL
     );
 
     /**
@@ -63,65 +106,81 @@ public class SkyGridGenerator extends ChunkGenerator {
      */
     private static final List<Material> FORCE_ITEM_LIST = Arrays.asList(
             Material.TORCH,
-            Material.REDSTONE_TORCH_OFF,
-            Material.REDSTONE_TORCH_ON,
-            Material.WATER_LILY,
-            Material.REDSTONE_COMPARATOR,
+            Material.REDSTONE_TORCH,
+            Material.LILY_PAD,
+            Material.COMPARATOR,
             Material.REDSTONE_WIRE,
             Material.REDSTONE,
-            Material.NETHER_WARTS,
-            Material.IRON_PLATE,
-            Material.STONE_PLATE,
-            Material.GOLD_PLATE,
-            Material.WOOD_PLATE,
-            Material.TRAP_DOOR,
+            Material.NETHER_WART,
+            Material.STONE_PRESSURE_PLATE,
             Material.ACACIA_DOOR,
             Material.BIRCH_DOOR,
             Material.DARK_OAK_DOOR,
             Material.IRON_DOOR,
             Material.JUNGLE_DOOR,
             Material.SPRUCE_DOOR,
-            Material.WOOD_DOOR,
-            Material.WOODEN_DOOR,
             Material.IRON_DOOR,
             Material.IRON_TRAPDOOR,
-            Material.SIGN,
-            Material.BANNER,
             Material.LADDER,
-            Material.VINE,
-            Material.SAPLING
+            Material.VINE
     );
+
+    private static boolean isForcedItem(final Material material) {
+        return Tag.DOORS.isTagged(material)
+            || Tag.TRAPDOORS.isTagged(material)
+            || Tag.SIGNS.isTagged(material)
+            || Tag.BANNERS.isTagged(material)
+            || Tag.SAPLINGS.isTagged(material)
+            || Tag.BUTTONS.isTagged(material)
+            || Tag.RAILS.isTagged(material)
+            || Tag.WOODEN_PRESSURE_PLATES.isTagged(material)
+            || Tag.STONE_PRESSURE_PLATES.isTagged(material)
+            || Tag.PRESSURE_PLATES.isTagged(material)
+            || Tag.BEDS.isTagged(material)
+            || FORCE_ITEM_LIST.contains(material);
+    }
 
     /**
      * List of creatures prohibited from spawning.
      */
-    private static final List<CreatureType> BAN_CREATURE_LIST = Arrays.asList(
-            CreatureType.ENDER_DRAGON,
-            CreatureType.GIANT
+    private static final List<EntityType> BAN_CREATURE_LIST = Arrays.asList(
+            EntityType.ENDER_DRAGON,
+            EntityType.GIANT
     );
 
     /**
      * Block to build the world.
      */
-    public static final Material[] BLOCKS = Arrays.stream(Material.values()).filter(material ->
-        material.isBlock() && material.isSolid() && !SkyGridGenerator.FORCE_ITEM_LIST.contains(material)
-        && !SkyGridGenerator.BAN_BLOCK_LIST.contains(material)
-    ).collect(Collectors.toList()).toArray(new Material[0]);
+    public static final Material[] BLOCKS = Arrays.stream(Material.values())
+        .filter(material -> !material.isLegacy())
+        .filter(material -> !material.isAir())
+        .filter(Material::isBlock)
+        .filter(Material::isSolid)
+        .filter(material -> !BAN_BLOCK_LIST.contains(material))
+        .filter(material -> !isBlockEntity(material) || KEEP_CONTAINERS.contains(material))
+        .filter(material -> !isForcedItem(material))
+        .toArray(Material[]::new);
 
     /**
      * Items to be placed in containers
      */
-    public static final Material[] ITEMS = Arrays.stream(Material.values()).filter(material ->
-        (!material.isBlock() || !material.isSolid() || SkyGridGenerator.FORCE_ITEM_LIST.contains(material))
-        && !SkyGridGenerator.BAN_BLOCK_LIST.contains(material)
-    ).collect(Collectors.toList()).toArray(new Material[0]);
+    public static final Material[] ITEMS = Arrays.stream(Material.values())
+        .filter(material -> !material.isLegacy())
+        .filter(material -> !material.isAir())
+        .filter(Material::isItem)
+        .filter(material -> !material.isBlock() || !material.isSolid()
+                            || isForcedItem(material) || isBlockEntity(material))
+        .filter(material -> !BAN_BLOCK_LIST.contains(material))
+        .toArray(Material[]::new);
 
     /**
      * Creature allowed to spawn with mob spawner.
      */
-    public static final CreatureType[] CREATURES = Arrays.stream(CreatureType.values()).filter(creature ->
-        !SkyGridGenerator.BAN_CREATURE_LIST.contains(creature)
-    ).collect(Collectors.toList()).toArray(new CreatureType[0]);
+    public static final EntityType[] CREATURES = Arrays.stream(EntityType.values())
+        .filter(creature -> creature.getEntityClass() != null)
+        .filter(creature -> Mob.class.isAssignableFrom(creature.getEntityClass()))
+        .filter(creature -> !SkyGridGenerator.BAN_CREATURE_LIST.contains(creature))
+        .toArray(EntityType[]::new);
 
     private final ContainerPopulator skyGridPopulator;
     private final SpawnerPopulator spawnerPopulator;
@@ -131,37 +190,71 @@ public class SkyGridGenerator extends ChunkGenerator {
         this.spawnerPopulator = new SpawnerPopulator();
     }
 
-    private void generatePlatform(byte[] bytes, int y) {
+    private void generatePlatform(final ChunkData chunkData, final int y) {
         for (int z = 0; z < 16; z++)
             for (int x = 0; x < 16; x++)
-                bytes[(x * 16 + z) * 256 + y] = (byte) Material.GLASS.getId();
+                chunkData.setBlock(x, y, z, Material.GLASS);
     }
 
-    private void generateSurface(byte[] bytes, Random random, int y) {
+    private void generateSkygridBlock(final ChunkData chunkData, final Material material, final int x, final int y, final int z) {
+        final BlockData blockData = material.createBlockData();
+        final Leaves leaves;
+        final Piston piston;
+
+        if (blockData == null) {
+             chunkData.setBlock(x, y, z, material);
+            return;
+        }
+
+        if (Tag.LEAVES.isTagged(material)) {
+            leaves = (Leaves) material.createBlockData();
+            leaves.setPersistent(true);
+        }
+
+        if (material == Material.PISTON) {
+            piston = (Piston) material.createBlockData();
+            piston.setExtended(false);
+        }
+
+        chunkData.setBlock(x, y, z, blockData);
+    }
+
+    private void generateSkyGrid(final ChunkData chunkData, final Random random, final int y) {
         for (int z = 0; z < 16; z += 4)
             for (int x = 0; x < 16; x += 4)
-                bytes[(x * 16 + z) * 256 + y] = (byte) SkyGridGenerator.BLOCKS[random.nextInt(SkyGridGenerator.BLOCKS.length)].getId();
+                 generateSkygridBlock(chunkData, SkyGridGenerator.BLOCKS[random.nextInt(SkyGridGenerator.BLOCKS.length)], x, y, z);
     }
 
     @Override
-    public byte[] generate(World world, Random randomSeed, int chunkX, int chunkZ) {
-        Random random = new Random();
-        byte[] result = new byte[16 * 16 * 256];
-        boolean surface = Math.abs(chunkX) <= 1 && Math.abs(chunkZ) <= 1;
-
-        for (int y = 0; y < world.getMaxHeight(); y += 4) {
-            if (surface && y == world.getMaxHeight() - 4)
-                generatePlatform(result, y);
+    public void generateNoise(final WorldInfo worldInfo, final Random randomSeed, final int chunkX, final int chunkZ, final ChunkData chunkData) {
+        final Random random = new Random();
+        final boolean surface = Math.abs(chunkX) <= 1 && Math.abs(chunkZ) <= 1;
+        
+        for (int y = 0; y < worldInfo.getMaxHeight(); y += 4) {
+            if (surface && y == worldInfo.getMaxHeight() - 4)
+                generatePlatform(chunkData, y);
             else
-                generateSurface(result, random, y);
+                generateSkyGrid(chunkData, random, y);
         }
-        return result;
+    }
+
+    @Override
+    public void generateBedrock(final WorldInfo worldInfo, final Random randomSeed, final int chunkX, final int chunkZ, final ChunkData chunkData) {
+
+    }
+
+    @Override
+    public void generateCaves(final WorldInfo worldInfo, final Random randomSeed, final int chunkX, final int chunkZ, final ChunkData chunkData) {
+
+    }
+
+    @Override
+    public void generateSurface(final WorldInfo worldInfo, final Random randomSeed, final int chunkX, final int chunkZ, final ChunkData chunkData) {
+
     }
 
     @Override
     public Location getFixedSpawnLocation(World world, Random randomSeed) {
-        Random random = new Random();
-
         return new Location(world, 0, world.getMaxHeight() - 2, 0);
     }
 
--- src/main/java/fr/mrcubee/skygrid/generator/populator/SpawnerPopulator.java
+++ src/main/java/fr/mrcubee/skygrid/generator/populator/SpawnerPopulator.java
@@ -4,8 +4,10 @@ import fr.mrcubee.skygrid.generator.SkyGridGenerator;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.generator.BlockPopulator;
+import org.bukkit.spawner.Spawner;
 
 import java.util.Random;
 
@@ -15,8 +17,10 @@ import java.util.Random;
  */
 public class SpawnerPopulator extends BlockPopulator {
 
-    private void populateSpawner(Random random, CreatureSpawner creatureSpawner) {
-        creatureSpawner.setCreatureType(SkyGridGenerator.CREATURES[random.nextInt(SkyGridGenerator.CREATURES.length)]);
+    private void populateSpawner(final Random random, final CreatureSpawner spawner) {
+        spawner.setSpawnCount(0);
+        spawner.setSpawnedType(random.nextBoolean() ? SkyGridGenerator.CREATURES[random.nextInt(SkyGridGenerator.CREATURES.length)] : null);
+        spawner.update(true, false);
     }
 
     private void populateSurface(Random random, Chunk chunk, int y) {
@@ -25,7 +29,7 @@ public class SpawnerPopulator extends BlockPopulator {
         for (int z = 0; z < 16; z += 4) {
             for (int x = 0; x < 16; x += 4) {
                 block = chunk.getBlock(x, y, z);
-                if (block.getType() == Material.MOB_SPAWNER)
+                if (block.getType() == Material.SPAWNER)
                     populateSpawner(random, (CreatureSpawner) block.getState());
             }
         }
@@ -33,7 +37,7 @@ public class SpawnerPopulator extends BlockPopulator {
 
     @Override
     public void populate(World world, Random randomSeed, Chunk chunk) {
-        Random random = new Random();
+        final Random random = new Random();
 
         for (int y = 0; y < world.getMaxHeight(); y += 4)
             populateSurface(random, chunk, y);
